9c9f9edfb94c8613b0d209cf53812501c4b32138
mariohernandez <mrhernandez@mednet.ucla.edu>
2024-06-28 17:03:46 -0700

    Updates stories

--- src/components/02-molecules/card/card.stories.jsx
+++ src/components/02-molecules/card/card.stories.jsx
@@ -2,10 +2,6 @@ import parse from 'html-react-parser';
 
 import card from './card.twig';
 import data from './card.yml';
-import dataHorizontal from './card-horizontal.yml';
-
-// If this component used JavaScript (card.js), we could import it as follows:
-// import './card'; // no extension needed.
 
 const component = {
   title: 'Molecules/Card',
